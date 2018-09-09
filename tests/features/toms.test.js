8c9ac480678566a9509710e3de9d3f85e1a77853
H.L <heng@hls-macbook-pro.home>
2018-09-09 19:58:16 +0200

    add first test case

---
+++ tests/features/toms.test.js
@@ -0,0 +1,19 @@
+import login from '../login';
+
+export default {
+    'Title is 2getThere': (browser) => {
+      // Browser is the browser that is being controlled
+      browser
+        .url('http://192.168.1.168:8620') // Navigate to the url
+        .waitForElementVisible('body', 1000) // Wait until you can see the body element.
+        .verify.title('2GT Demo - Index') // Verify that the title is 'Bing'
+        .end() // This must be called to close the browser at the end
+      },
+      'Terminal typing': (browser) => {
+        const terminalSelector = 'input.terminal-input';
+        login(browser, 'http://192.168.1.168:8620/inspector/#!/terminal')
+          .waitForElementVisible('.terminal-viewport', 1000)
+          .setValue(terminalSelector, 'run your script')
+          .end()
+      }
+  }
---
+++ tests/login.js
@@ -0,0 +1,10 @@
+export default function login(browser, url) {
+    browser
+        .url(url)
+        .click('li[ng-hide="user.isLoggedIn"]')
+        .waitForElementVisible('.modal-dialog', 1000)
+        .setValue('input[name=username]', 'admin')
+        .setValue('input[name=password]', 'admin123')
+        .click('.modal-footer button[type="submit"]')
+        return browser
+}
